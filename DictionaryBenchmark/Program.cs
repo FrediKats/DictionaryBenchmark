3bbcf7b619fb66ca72f439efd2ae684e8fe1c355
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-04-03 14:01:56 +0300

    Add project files.

---
+++ DictionaryBenchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DictionaryBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
